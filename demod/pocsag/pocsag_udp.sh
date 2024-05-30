feaf18c762c9a27819cb7ea1e2442c994bdfe324
Noah Burget <nburget1@hotmail.com>
2024-05-29 20:35:07 -0400

    pocsag decode using command line tools

--- demod/pocsag/pocsag_udp.sh
+++ demod/pocsag/pocsag_udp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+####################
+# This script simply uses netcat to listen on port 7355 (UDP) for I/Q data streams
+# Then sox is used to parse the raw data into audio data
+# Multimon-ng is used to decode this audio data
+####################
+
 PORT=7355
 
 nc -l -u -p ${PORT} | \
